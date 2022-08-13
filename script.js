42c4fa66c62784848eb2e8f64a32db04740cfbbb
MVIVN <mvngwerume@gmail.com>
2022-08-14 04:19:06 +1200

    Added some functions which aren't working correctly, to be fixed

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ function createGrid(length) {
     document.getElementById('grid-container').style.gridTemplateColumns = `repeat(${length}, ${650/length}px`;
     document.getElementById('grid-container').style.gridTemplateRows = `repeat(${length}, ${650/length}px`;
     for (i = 1; i <= (length*length); i++) {
-        let gridBox = document.createElement('div');
-        gridBox.setAttribute('class', 'grid-item');
-        gridContainer.appendChild(gridBox);
+        let box = document.createElement('div');
+        box.setAttribute('class', 'grid-item');
+        gridContainer.appendChild(box);
     }
 }
 
-createGrid(10);
+createGrid(16);
+
+const gridBox = document.querySelectorAll('div.grid-item');
+console.log(gridBox);
+gridBox.forEach(addEventListener('mouseover', draw));
+
+function draw() {
+    for (i = 0; i < gridBox.length; i++) {
+        gridBox[i].classList.add('colored-in');
+    };
+}
