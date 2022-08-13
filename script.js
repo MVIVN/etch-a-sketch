14aeb7f5857f1f3dfe057cb68be38c84b15e7411
Vee Ngwerume <geekeritch@gmail.com>
2022-08-14 05:03:53 +1200

    Add a new div which will serve as a control box for setting parameters

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const gridContainer = document.querySelector('.grid-container');
-gridContainer.setAttribute('id', 'grid-container');
+const gridContainer = document.querySelector('#grid-container');
 
+createGrid(60);
+
+const gridBox = document.querySelectorAll('div.grid-item'); //nodelist
+gridBox.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.setAttribute('style', 'background-color: black');
+    })
+});
+
+
+
+// Functions
 function createGrid(length) {
     document.getElementById('grid-container').style.gridTemplateColumns = `repeat(${length}, ${650/length}px`;
     document.getElementById('grid-container').style.gridTemplateRows = `repeat(${length}, ${650/length}px`;
@@ -10,12 +21,3 @@ function createGrid(length) {
         gridContainer.appendChild(box);
     }
 }
-
-createGrid(16);
-
-const gridBox = document.querySelectorAll('div.grid-item'); //nodelist
-gridBox.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.setAttribute('style', 'background-color: black');
-    })
-});
